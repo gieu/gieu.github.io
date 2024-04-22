bf9a0da93d5ae335cebe379897f6994e4638aa71
Sonya <sonya.castro.gomez@gmail.com>
2024-04-22 15:31:26 -0500

    fix: site + base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://grupoinformaticaeducativa.uninorte.edu.co',
+  base: '/gieu',
   server: { port: 8517, host: true },
   adapter: node({
     mode: 'standalone',
